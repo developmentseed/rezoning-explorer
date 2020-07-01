5c36f09300d1cc9cb95c8485a0423f5e63f4db93
Jeevan Farias <jeevan@developmentseed.org>
2020-07-01 15:36:54 -0400

    Add jest extensions

--- jest.config.js
+++ jest.config.js
@@ -72,24 +72,26 @@ module.exports = {
   // An array of directory names to be searched recursively up from the requiring module's location
 
   moduleDirectories: [
-    'node_modules'
+    'node_modules',
+    'app/assets/scripts'
   ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "json",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'ts',
+    'tsx',
+    'node'
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy'
   },
 
+  // import Header from '../common/page-header';
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
