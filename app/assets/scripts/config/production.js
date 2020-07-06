9ad26fb2f4fa17abb70569f84bdb48e1f97b5b5e
Vitor George <vitor.george@gmail.com>
2020-07-06 17:17:25 +0100

    Add app short title

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -3,5 +3,6 @@
 export default {
   environment: 'production',
   appTitle: 'REZoning Web',
+  appShortTitle: 'REZoning Web',
   mbToken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJja2MyYzVsNjAxNXd2MnFwOWV0bHBjaWxsIn0.mJzFaMqKKSOkwfXCt00cww'
 };
