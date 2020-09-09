d130036f759a7b5ca0a74b07019b3def4f0863b8
Vitor George <vitor.george@gmail.com>
2020-09-09 17:41:00 +0100

    Make first section occupy all space necessary

--- app/assets/scripts/components/explore/zone-analysis-panel.js
+++ app/assets/scripts/components/explore/zone-analysis-panel.js
@@ -8,7 +8,7 @@ const PanelInner = styled.div`
   padding: 1rem 0;
   flex: 1;
   display: grid;
-  grid-template-rows: 1fr 1fr auto;
+  grid-template-rows: auto 1fr;
 `;
 
 function ZoneAnalysisPanel (props) {
