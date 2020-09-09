610b5b2cbabb15b0bc038695e84cab17db48b497
Vitor George <vitor.george@gmail.com>
2020-09-09 11:04:22 +0100

    Fetch zone scores and populate side panel

---
+++ app/assets/scripts/context/fetch-zones.js
@@ -0,0 +1,73 @@
+import * as topojson from 'topojson-client';
+import { fetchJSON } from '../context/reduxeed';
+import config from '../config';
+
+const { apiEndpoint } = config;
+
+async function getZoneSummary (feature, options) {
+  const zoneSummary = {
+    id: feature.properties.id,
+    name: feature.properties.name,
+    color: '#2c2a59'
+  };
+  try {
+    const { body } = await fetchJSON(
+      `${apiEndpoint}/zone?filters=0,1000000|10000,1000000|0,1000000|0,1000000|0,5000`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          aoi: feature.geometry,
+          weights: {},
+          lcoe: {}
+        })
+      }
+    );
+
+    // Add analysis to summary
+    zoneSummary.analysis = body;
+  } catch (error) {
+    // eslint-disable-next-line
+    console.log(`Error fetching zone ${zoneSummary.id} analysis.`);
+  }
+
+  return zoneSummary;
+}
+
+export default async function generateZones (areaId, options) {
+  // Get area topojson
+  const { body: allZonesTopoJSON } = await fetchJSON(
+    `/public/zones/${areaId}.topojson`
+  );
+
+  // Parse topojson
+  const { features: allZones } = topojson.feature(
+    allZonesTopoJSON,
+    allZonesTopoJSON.objects[areaId]
+  );
+
+  // Fetch Lcoe for each sub-area
+  const results = await Promise.all(
+    allZones.map((z) =>
+      getZoneSummary(z, {
+        weights: {},
+        lcoe: {}
+      })
+    )
+  );
+
+  const maxScore = Math.max(
+    ...results.map(({ analysis }) => (analysis ? analysis.zone_score : 0))
+  );
+
+  return results.map((z) => {
+    if (!z.analysis) return z;
+
+    return {
+      ...z,
+      analysis: {
+        ...z.analysis,
+        zone_score: (z.analysis.zone_score / maxScore)
+      }
+    };
+  });
+}
