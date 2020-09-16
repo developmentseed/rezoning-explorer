e9d63aac84d885788dd55bd07b9acfeabfc2d02a
Jeevan Farias <jeevan@developmentseed.org>
2020-09-16 13:00:47 -0400

    Allow 0 weights, round slider value

--- app/assets/scripts/components/explore/panel-data.js
+++ app/assets/scripts/components/explore/panel-data.js
@@ -45,16 +45,16 @@ export const lcoeList = [
 ];
 
 export const weightsList = [
-  { name: 'LCOE Generation', id: 'lcoe_gen', range: [0.1, 1] },
-  { name: 'LCOE Transmission', id: 'lcoe_transmission', range: [0.1, 1] },
-  { name: 'LCOE Road', id: 'lcoe_road', range: [0.1, 1] },
-  { name: 'Distance to Load Centers', id: 'distance_load', range: [0.1, 1] },
-  { name: 'Technology Co-Location', id: 'technology_colocation', range: [0.1, 1] },
-  { name: 'Human Footprint', id: 'human_footprint', range: [0.1, 1] },
-  { name: 'Population Density', id: 'pop_density', range: [0.1, 1] },
-  { name: 'Slope', id: 'slope', range: [0.1, 1] },
-  { name: 'Land Use Score', id: 'land_use', range: [0.1, 1] },
-  { name: 'Capacity Value (Wind Only)', id: 'capacity_value', range: [0.1, 1] }
+  { name: 'LCOE Generation', id: 'lcoe_gen', range: [0, 1] },
+  { name: 'LCOE Transmission', id: 'lcoe_transmission', range: [0, 1] },
+  { name: 'LCOE Road', id: 'lcoe_road', range: [0, 1] },
+  { name: 'Distance to Load Centers', id: 'distance_load', range: [0, 1] },
+  { name: 'Technology Co-Location', id: 'technology_colocation', range: [0, 1] },
+  { name: 'Human Footprint', id: 'human_footprint', range: [0, 1] },
+  { name: 'Population Density', id: 'pop_density', range: [0, 1] },
+  { name: 'Slope', id: 'slope', range: [0, 1] },
+  { name: 'Land Use Score', id: 'land_use', range: [0, 1] },
+  { name: 'Capacity Value (Wind Only)', id: 'capacity_value', range: [0, 1] }
 ];
 
 export const presets = {
