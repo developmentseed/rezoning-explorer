bb57942201fa904ea7bf9ee3b14c4ba35770471f
Jeevan Farias <jeevan@developmentseed.org>
2020-11-05 13:48:31 -0500

    Fix history refresh issue

--- app/assets/scripts/context/explore-context.js
+++ app/assets/scripts/context/explore-context.js
@@ -37,45 +37,38 @@ const qsStateHelper = new QsState({
 });
 
 export function ExploreProvider (props) {
-  const history = useHistory();
+  //const history = useHistory();
   const location = useLocation();
 
   const qsState = qsStateHelper.getState(location.search.substr(1));
-  const initQsState = qsStateHelper.getState(location.search.substr(1));
-
-  /*
-  const [selectedView, setSelectedView] = useQsState({
-    key: 'view',
-    default: 'imagery',
-    validator: ['imagery', 'scenes']
-  });*/
 
   const [selectedArea, setSelectedArea] = useState(null);
-  //const [selectedAreaId, setSelectedAreaId] = useState(qsState.areaId);
 
   const [selectedAreaId, setSelectedAreaId] = useQsState({
     key: 'areaId',
     default: undefined
-  })
+  });
+
   const [showSelectAreaModal, setShowSelectAreaModal] = useState(
     !qsState.areaId
   );
+
   const [areas, setAreas] = useState([]);
 
   const [map, setMap] = useState(null);
+
   useEffect(() => {
     setSelectedArea(areas.find((a) => a.id === selectedAreaId));
   }, [selectedAreaId]);
 
-  //const [selectedResource, setSelectedResource] = useState(qsState.resourceId);
   const [selectedResource, setSelectedResource] = useQsState({
     key: 'resourceId',
     default: undefined
-  })
+  });
+
   const [showSelectResourceModal, setShowSelectResourceModal] = useState(
     !qsState.resourceId
   );
-  // const [areaTypeFilter, setAreaTypeFilter] = useState(energyAreaTypeMap[selectedResource] || energyAreaTypeMap.default);
 
   const [gridMode, setGridMode] = useState(false);
   const [gridSize, setGridSize] = useState(GRID_OPTIONS[0]);
@@ -151,51 +144,8 @@ export function ExploreProvider (props) {
   }, [tourStep]);
 
   useEffect(() => {
-    let overrideId;
-
-    /*
-    const nextFilter = energyAreaTypeMap[selectedResource];
-
-    if (nextFilter) {
-      setAreaTypeFilter(nextFilter);
-
-      if (selectedArea && !nextFilter.includes(selectedArea.type)) {
-        overrideId = null;
-      }
-    }
-      */
-
-    const qString = qsStateHelper.getQs({
-      //areaId: overrideId === undefined ? selectedAreaId : overrideId,
-      resourceId: selectedResource
-    });
-
-    // Push params as new URL, if different from current URL
-    if (qString !== location.search.substr(1)) {
-      //history.push({ search: qString });
-    }
-  }, [selectedAreaId, selectedResource]);
-
-  useEffect(() => {
     dispatchCurrentZones({ type: 'INVALIDATE_FETCH_ZONES' });
   }, [selectedAreaId]);
-
-  // Update context on URL change
-  useEffect(() => {
-    const { areaId, resourceId } = qsStateHelper.getState(
-      location.search.substr(1)
-    );
-
-    if (areaId !== selectedAreaId) {
-      //setSelectedAreaId(areaId);
-      //setShowSelectAreaModal(!areaId);
-    }
-
-    if (resourceId !== selectedResource) {
-      //setSelectedResource(resourceId);
-      //setShowSelectResourceModal(!resourceId);
-    }
-  }, [location.search]);
 
   const [inputTouched, setInputTouched] = useState(true);
   const [zonesGenerated, setZonesGenerated] = useState(false);
