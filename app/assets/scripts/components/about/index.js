d5d3f23185a868c48fd36a6b4e39914bda2f0e88
Vitor George <vitor.george@gmail.com>
2020-12-23 11:33:53 +0000

    Update links to open in new tab/window

--- app/assets/scripts/components/about/index.js
+++ app/assets/scripts/components/about/index.js
@@ -98,7 +98,7 @@ function Explore () {
         <InpageBody>
           <AboutPageBodyInner>
             <p>The Renewable Energy Zoning (REZoning) tool is an interactive platform designed to identify, visualize and rank zones that are most suitable for the development of solar, wind or offshore wind projects. Users can customize the spatial filters and economic parameters to meet their specific needs or to best represent the country context.</p>
-            <p>Inspired by <a href='https://mapre.lbl.gov/'>Berkley’s MapRE</a> and developed by <a href='https://esmap.org/'>ESMAP</a> the tool bring together complex spatial analysis and economic calculations into an online, user-friendly environment that allows users and decision makers to obtain insights into the technical and economic potential of renewable energy resources for any country.</p>
+            <p>Inspired by <a rel='noreferrer' target='_blank' href='https://mapre.lbl.gov/'>Berkley’s MapRE</a> and developed by <a rel='noreferrer' target='_blank' href='https://esmap.org/'>ESMAP</a> the tool bring together complex spatial analysis and economic calculations into an online, user-friendly environment that allows users and decision makers to obtain insights into the technical and economic potential of renewable energy resources for any country.</p>
             <p>
             The REZoning tool is powered by global geospatial datasets and baseline assumptions for the economic parameters. No input dataset, nor simulation outcome produced by the tool represents the official position of the World Bank Group. A list of the input datasets can be found here (link to ED.info datasets) and the methodology is thoroughly described here (link to github).
             </p>
