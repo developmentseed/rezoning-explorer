03a6de570adf71fa8d36c8a9945cc9c21210a834
Lane Goodman <Lane@developmentseed.org>
2021-03-30 00:52:26 -0400

    Add output filters as component in secondary panel

---
+++ app/assets/scripts/components/explore/output-filters.js
@@ -0,0 +1,101 @@
+import React, { useContext, useCallback } from 'react';
+import {
+  FormGroupWrapper,
+  PanelOption,
+  OptionHeadline,
+  PanelOptionTitle,
+  InactiveMessage
+} from '../../styles/form/form';
+import FormInput from './form/form-input';
+import {
+  Accordion,
+  AccordionFold,
+  AccordionFoldTrigger
+} from '../../components/accordion';
+import Heading from '../../styles/type/heading';
+import { makeTitleCase } from '../../styles/utils/general';
+import InfoButton from '../common/info-button';
+
+import ExploreContext from '../../context/explore-context';
+
+function OutputFilters (props) {
+  const { maxZoneScore, setMaxZoneScore, maxLCOE, setMaxLCOE } = useContext(
+    ExploreContext
+  );
+
+  const outputFilters = [
+    [maxZoneScore, setMaxZoneScore, 'Run analysis to filter on zone score'],
+    [maxLCOE, setMaxLCOE, 'Run analysis to filter on LCOE']
+  ];
+
+  return (
+    <Accordion>
+      {({ checkExpanded, setExpanded }) => (
+        <AccordionFold
+          forwardedAs={FormGroupWrapper}
+          isFoldExpanded={checkExpanded(0)}
+          setFoldExpanded={(v) => setExpanded(0, v)}
+          renderHeader={({ isFoldExpanded, setFoldExpanded }) => (
+            <AccordionFoldTrigger
+              isExpanded={isFoldExpanded}
+              onClick={() => setFoldExpanded(!isFoldExpanded)}
+            >
+              <Heading size='small' variation='primary'>
+                {makeTitleCase('Filter Results')}
+              </Heading>
+            </AccordionFoldTrigger>
+          )}
+          renderBody={({ isFoldExpanded }) => (
+            <>
+              {outputFilters.map(
+                ([filterObject, setFilterObject, inactiveMessage]) => {
+                  const onChange = useCallback(
+                    (value) => {
+                      setFilterObject({
+                        ...filterObject,
+                        input: {
+                          ...filterObject.input,
+                          value
+                        }
+                      });
+                    },
+                    [filterObject]
+                  );
+                  return (
+                    <PanelOption
+                      key={filterObject.name}
+                      hidden={!isFoldExpanded}
+                    >
+                      <OptionHeadline>
+                        <PanelOptionTitle>
+                          {`${filterObject.name}`.concat(
+                            filterObject.unit ? ` (${filterObject.unit})` : ''
+                          )}
+                        </PanelOptionTitle>
+                        {filterObject.info && (
+                          <InfoButton
+                            info={filterObject.info}
+                            id={filterObject.name}
+                          >
+                            Info
+                          </InfoButton>
+                        )}
+                      </OptionHeadline>
+                      {filterObject.active ? (
+                        <FormInput option={filterObject} onChange={onChange} />
+                      ) : (
+                        <InactiveMessage>{inactiveMessage}</InactiveMessage>
+                      )}
+                    </PanelOption>
+                  );
+                }
+              )}
+            </>
+          )}
+        />
+      )}
+    </Accordion>
+  );
+}
+
+export default OutputFilters;
