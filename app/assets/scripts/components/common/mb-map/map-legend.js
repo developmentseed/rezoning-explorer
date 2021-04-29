f872c14a548378433f3dfd42a5c8705e19b6c9f1
Jeevan Farias <jeevan@developmentseed.org>
2021-04-29 09:53:31 -0400

    Fix proptype warning

--- app/assets/scripts/components/common/mb-map/map-legend.js
+++ app/assets/scripts/components/common/mb-map/map-legend.js
@@ -208,7 +208,7 @@ function RasterLegendItem({ mapLayers, filterRanges, filtersLists }) {
 
 RasterLegendItem.propTypes = {
   mapLayers: T.array,
-  filterRanges: T.array,
+  filterRanges: T.object,
   filtersLists: T.array
 };
 
@@ -344,5 +344,5 @@ MapLegend.propTypes = {
   selectedResource: T.string.isRequired,
   mapLayers: T.array.isRequired,
   filtersLists: T.array.isRequired,
-  filterRanges: T.array.isRequired
+  filterRanges: T.object.isRequired
 };
