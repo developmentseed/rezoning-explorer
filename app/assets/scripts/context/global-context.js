45d2d1ae28620a9d4fbb8f6ecfa61eb985226a44
Vitor George <vitor.george@gmail.com>
2021-04-29 10:18:47 +0100

    Remove window height listener from GlobalContext

--- app/assets/scripts/context/global-context.js
+++ app/assets/scripts/context/global-context.js
@@ -11,16 +11,6 @@ const {
 const GlobalContext = createContext({});
 
 export function GlobalProvider (props) {
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      // Store the height to set the page min height. This is needed for mobile
-      // devices to account for the address bar, since 100vh does not work.
-      // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-      setWindowHeight(window.innerHeight);
-    });
-  }, []);
 
   // The user is restricted to one one download at a time, Object 'downloadTask'
   // keeps metadata of active download.
@@ -91,7 +81,6 @@ export function GlobalProvider (props) {
     <>
       <GlobalContext.Provider
         value={{
-          windowHeight,
           setDownload
         }}
       >
--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,10 +1,10 @@
 import './wdyr';
 import '@babel/polyfill';
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import GlobalContext, { GlobalProvider } from './context/global-context';
+import { GlobalProvider } from './context/global-context';
 import history from './utils/history.js';
 
 import GlobalStyles from './styles/global';
@@ -17,16 +17,28 @@ import theme from './styles/theme/theme';
 import Home from './components/home';
 import Explore from './components/explore';
 import About from './components/about';
+import { useState } from 'react';
 
 // Root component.
-function Root () {
-  const { windowHeight } = useContext(GlobalContext);
+function Root() {
+  const [windowHeight, setWindowHeight] = useState(null);
+
+  const updateWindowHeight = () => {
+    // Store the height to set the page min height. This is needed for mobile
+    // devices to account for the address bar, since 100vh does not work.
+    // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    setWindowHeight(window.innerHeight);
+  };
 
   useEffect(() => {
     // Hide the welcome banner.
     const banner = document.querySelector('#welcome-banner');
     banner.classList.add('dismissed');
     setTimeout(() => banner.remove(), 500);
+    window.addEventListener('resize', updateWindowHeight);
+    return () => {
+      window.removeEventListener(updateWindowHeight);
+    };
   }, []);
 
   return (
