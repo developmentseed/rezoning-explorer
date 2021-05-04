a84627bdfde2d025bee49caefaff5d11a2134b0c
Jeevan Farias <jeevan@developmentseed.org>
2021-05-04 14:42:27 -0400

    Fix non summing of locked

--- app/assets/scripts/components/explore/form/weights-form.js
+++ app/assets/scripts/components/explore/form/weights-form.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import T from 'prop-types';
 import {
   FormWrapper,
@@ -7,18 +7,17 @@ import {
   OptionHeadline
 } from '../../../styles/form/form';
 import FormIntro from './form-intro';
-import InfoButton from '../../common/info-button';
 import FormInput from '../form/form-input';
 import { distributedDivision, sumBy } from '../../../utils/math';
 
 function updateWeight(weights, id, value) {
   const [w, setValue] = weights.find(([w]) => w.id === id);
-  console.log({ w, value });
+  // console.log({ w, value });
   setValue({
     ...w,
     input: {
       ...w.input,
-      value: value > 100 ? 1 : value > 0 ? Math.round(value) / 100 : 0
+      value: value > 100 ? 1 : value > 0 ? Math.round(value) : 0
     }
   });
 }
@@ -29,7 +28,7 @@ function WeightsForm(props) {
   const [weightsLocks, setWeightLocks] = useState({});
 
   function onSliderChange(id, sliderVal) {
-    console.log(id, sliderVal);
+    // console.log(id, sliderVal);
 
     let updatedValuesArray = weights.map(([w]) => {
       return {
@@ -38,41 +37,46 @@ function WeightsForm(props) {
         value: id === w.id ? sliderVal : w.input.value
       };
     });
-    console.log({ updatedValuesArray });
+    // console.log({ updatedValuesArray });
 
     // Sliders to update. Everyone except the disabled ones and the current.
     const slidersToUpdate = updatedValuesArray.filter(
       (slider) => !slider.locked && slider.id !== id && slider.value > 0
     );
-    console.log({ slidersToUpdate });
+    // console.log({ slidersToUpdate });
 
     // Get by how much is over 100;
     const excess = 100 - sumBy(updatedValuesArray, 'value');
-    console.log({ excess });
     // By how much we need to update the sliders.
     // Since the steps are integers the deltas is an array with the value to
     // use to update each of the indexes.
     const deltas = distributedDivision(excess, slidersToUpdate.length);
-    console.log({ deltas });
+    // console.log({ deltas });
 
     // Update the values of the other sliders.
-    updatedValuesArray = slidersToUpdate.map((slider, i) => {
-      // Calculate the new value and keep it between 0 - 100.
-      const newVal = Math.max(0, Math.min(slider.value + deltas[i], 100));
-      // If the sliding slider reached 100 then this is 0.
-      // Otherwise use the value.
-      return {
-        slider,
-        value: newVal
-      };
+    updatedValuesArray = updatedValuesArray.map((slider, i) => {
+      // If this slider is not locked, we can update the value
+      if (slidersToUpdate.find(s => s.id === slider.id)) {
+        const deltaIndex = slidersToUpdate.findIndex(s => s.id === slider.id);
+        // Calculate the new value and keep it between 0 - 100.
+        const newVal = Math.max(0, Math.min(slider.value + deltas[deltaIndex], 100));
+        // If the sliding slider reached 100 then this is 0.
+        // Otherwise use the value.
+        return {
+          ...slider,
+          value: newVal
+        };
+      } else {
+        // Otherwise just use the locked value
+        return slider;
+      }
     });
-    console.log({ updatedValuesArray });
 
     // Total of other sliders.
     const otherTotalVal = sumBy(updatedValuesArray, (val) =>
       val.id === id ? 0 : val.value
     );
-    console.log({ otherTotalVal });
+
     // Allowed value to ensure that the sum doesn't go over or below 100.
     const allowedSliderVal = 100 - otherTotalVal;
 
@@ -85,7 +89,7 @@ function WeightsForm(props) {
     });
   }
 
-  console.log(weights);
+  // console.log(weights);
 
   return (
     <FormWrapper active={active}>
@@ -109,7 +113,7 @@ function WeightsForm(props) {
               }}
               option={{ ...weight, active: !weightsLocks[weight.id] }}
               onChange={(value) =>
-                onSliderChange(weight.id, Math.round(value * 100))}
+                onSliderChange(weight.id, Math.round(value))}
             />
           </PanelOption>
         );
