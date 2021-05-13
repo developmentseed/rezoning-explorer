f828c7efd1651bd6e2dce7bbe4124531a8b9aaef
Jeevan Farias <jeevan@developmentseed.org>
2021-05-13 15:03:13 -0400

    Lint

--- app/assets/scripts/components/common/mb-map/mb-map.js
+++ app/assets/scripts/components/common/mb-map/mb-map.js
@@ -329,7 +329,6 @@ const addInputLayersToMap = (map, layers, selectedArea, resource) => {
         map.setLayoutProperty(layerId, 'visibility', 'visible');
       } else {
         map.setLayoutProperty(layerId, 'visibility', 'none');
-        console.log('set not visible', layerId, layer.name)
       }
       return;
     }
@@ -457,7 +456,7 @@ function MbMap (props) {
       addInputLayersToMap(map, initializedLayers, selectedArea, selectedResource);
 
       const _output = outputLayers.map(l => {
-        map.setLayoutProperty(l.id, 'visibility', 'none')
+        map.setLayoutProperty(l.id, 'visibility', 'none');
         return ({
           ...l,
           disabled: l.category === 'output',
