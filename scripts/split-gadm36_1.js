a9c79b355d718a326e9bb6de9b67aaa95c8d1254
Vitor George <vitor.george@gmail.com>
2021-06-24 12:22:25 +0100

    Offset polygons position on antimeridian countries

--- scripts/split-gadm36_1.js
+++ scripts/split-gadm36_1.js
@@ -8,9 +8,40 @@ const regionsDir = process.argv[3];
 let currentCountry = null;
 let features = [];
 
-function writeCurrent () {
+const antimeridianCountries = ['USA', 'RUS', 'KIR', 'FJI'];
+
+// Recursive function to move point of polygons to the west
+// if country crosses antimeridian
+function correctPolygon(arrayElement) {
+  // Check if arrayElement is a coordinate pair
+  if (
+    arrayElement.length === 2 &&
+    typeof arrayElement[0] === 'number' &&
+    typeof arrayElement[1] === 'number'
+  ) {
+    return arrayElement[0] > 0
+      ? [arrayElement[0] - 360, arrayElement[1]]
+      : arrayElement;
+  }
+
+  return arrayElement.map((el) => correctPolygon(el));
+}
+
+function writeCurrent() {
   console.log(`writing ${currentCountry}`); // eslint-disable-line
+
+  if (antimeridianCountries.includes(currentCountry)) {
+    features = features
+      .map((f) => {
+        f.geometry.coordinates = f.geometry.coordinates.map((polygon) =>
+          correctPolygon(polygon)
+        );
+        return f;
+      });
+  }
+
   const fc = { type: 'FeatureCollection', features };
+
   fs.writeFileSync(
     path.join(regionsDir, `${currentCountry}.geojson`),
     JSON.stringify(fc)
